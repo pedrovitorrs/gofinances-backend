2ca3dbba7f9abf41dcf4c8eff4ea8f24a8285a25
pedrovitorrs <pedro442.rodrigues@gmail.com>
2023-04-03 22:37:33 -0300

    test: adicionando teste para user.

---
+++ internal/api/v1/repository/test/main_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+	db "github.com/pedrovitorrs/gofinances-backend/internal/api/v1/repository/sqlc"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:password@localhost:5432/go_finance?sslmode=disable"
+)
+
+var testQueries *db.Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+	log.Print("teste")
+	testQueries = db.New(conn)
+	os.Exit(m.Run())
+}
---
+++ internal/api/v1/repository/test/user_test.go
@@ -0,0 +1,56 @@
+package test
+
+import (
+	"context"
+	"testing"
+
+	repository "github.com/pedrovitorrs/gofinances-backend/internal/api/v1/repository/sqlc"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomUser(t *testing.T) repository.User {
+	arg := repository.CreateUserParams{
+		Username: "test",
+		Password: "123456",
+		Email:    "teste@gmail.com",
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.Username, user.Username)
+	require.Equal(t, arg.Password, user.Password)
+	require.Equal(t, arg.Email, user.Email)
+	require.NotEmpty(t, user.CreatedAt)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	createRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	userRandom := createRandomUser(t)
+	userDbGenerated, err := testQueries.GetUser(context.Background(), userRandom.Username)
+	require.NoError(t, err)
+	require.NotEmpty(t, userDbGenerated)
+
+	require.Equal(t, userRandom.Username, userDbGenerated.Username)
+	require.Equal(t, userRandom.Password, userDbGenerated.Password)
+	require.Equal(t, userRandom.Email, userDbGenerated.Email)
+	require.NotEmpty(t, userDbGenerated.CreatedAt)
+}
+
+func TestGetUserById(t *testing.T) {
+	userRandom := createRandomUser(t)
+	userDbGenerated, err := testQueries.GetUserById(context.Background(), userRandom.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, userDbGenerated)
+
+	require.Equal(t, userRandom.Username, userDbGenerated.Username)
+	require.Equal(t, userRandom.Password, userDbGenerated.Password)
+	require.Equal(t, userRandom.Email, userDbGenerated.Email)
+	require.NotEmpty(t, userDbGenerated.CreatedAt)
+}
